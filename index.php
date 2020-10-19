ecaa0e708e69d9ae6ae630985a69bf63f09c5537
CommonMatter <hoffmelanie@gmail.com>
2020-10-19 04:00:36 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+get_template_part('head');
+?>
+
+<?php
+get_template_part('header');
+?>
+
+<div class="center-pane">
+
+<div id="zoom-container">
+	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://digitallovelanguages.github.io/">Landscapes of Love Webzine</a><br />
+	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://rcravago.github.io/handle-with-care/inner-package.html">Love Letter to Digital Love Languages</a><br />
+	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://rcravago.github.io/handle-with-care/inner-package.html">Digital Love Languages on Are.na</a><br />
+	
+</div>
+
+	<div>
+		<div class="star-container">
+		<div class="circle">
+			<div class="border"></div>
+		<div id="rsvp">
+		<span class="zoom">Sharing Sessions Recordings:</span>
+	<ul>
+	<li>
+	<a target="_blank" class="zoom underline--dotted" href="https://www.youtube.com/watch?v=FyqOiIcCiaQ&t=398s">Tuesday</a>
+	</li>
+	<li>
+	<a target="_blank" class="zoom underline--dotted" href="https://www.youtube.com/watch?v=gAJ9akQceAA&t=3s">Wednesday</a>
+	</li>
+	</ul>
+	</div>
+	
+	<!-- <div class="star-12"></div> -->
+</div>
+	</div>
+</div>
+	<img id="sharing-poster"src="<?php echo get_bloginfo('template_directory'); ?>/img/site.png"</img>
+	<img class="main-image" src=<?php echo get_bloginfo('template_directory'); ?>/img/dll.png></img>
+
+	<!-- wp:paragraph -->
+	<div class="description">
+<p>🦋&nbsp;&nbsp;Welcome to the online hub of Digital Love Languages, a 10 week online class at the <a href="https://sfpc.io/digitallove-spring2020/">School for Poetic Computation</a> stewarded by Melanie Hoff, Max Fowler, Adina Glickstein, and Amber Officer-Narvasa.<br />  <br />
+	🦋&nbsp;&nbsp;As our daily activities and closest relationships become increasingly bound up with corporate systems of surveillance and exploitation, let’s explore and cultivate code as a love language that can be gentle, healing, and intimate. How do we want to live in a post COVID-19 world, and what role do we want technology to play? Digital Love Languages: Codes of Affirmation is a class about building poetic tools for online communion through a re-introduction to computers. This class is also a call to action for expanding computation’s capacity for fostering interdependence and feeling.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>🦋&nbsp;&nbsp;Together we will build small and personal software for affirming one another across physical distance. Technology will be unpacked as a social process, not only a logic or material, regarding code as a craft and medium capable of expressing the full range of feeling and desire.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>🦋&nbsp;&nbsp;This course covers the building blocks of programming and natural language processing using Python & Javascript in the browser. Networks of people and computers become fertile sites for ambient and playful communication. We will look to the history of the love letter as a form, from the&nbsp;<em class="special">quill</em>&nbsp;to the&nbsp;<em class="special">sext</em>.<br />
+Through coding with langugage and in the browser, we can facilitate a kind of care that happens in small moments of routine.</p>
+<br /><br />
+<p>
+	<a class="big-link" href="/syllabus">Syllabus</a>
+</p>
+<br /><br />
+<p class="special">🩸&nbsp;&nbsp;What if all the software we used was made by people who love us?</p>
+	</div>
+<!-- /wp:paragraph -->
+</div>
+
+<?php
+get_template_part('foot');
+?>
